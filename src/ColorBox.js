9b58b1ec7b98fc5789db50d5d1b61327f421534f
Hannah Malm <hannahemalm616@gmail.com>
2021-09-11 15:05:19 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class ColorBox extends Component {
 
-  state = {
-    todos: [
-      
-    ]
-  }
-
-  render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
-  }
-
+//If the opacity value is greater than or equal to 0.2:
+//the ColorBox component should render another ColorBox inside itself (recursive components!)
+//an opacity prop should be passed to the child
+//he passed opacity prop should be reduced by 0.1
+render() {
+    if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )
+    } else {
+      //If the opacity value is less than 0.2:
+      //do not render another ColorBox 
+      //instead, the render method should return null
+      return null
+    }
 }
-
+}
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,23 @@
 //your code here
+import React, { Component } from 'react';
+
+
+//You need to write export default calss or else youll get a warning
+//commentText renders from blog post
+//export the class so it can be used in other files
+//It should expect a single prop (the text of a comment), which can be used in the component via: this.props.commentText
+//This prop is passed in src/BlogPost.js - the comment text is passed into the blog post
+//It should have a single <div> in its render() method
+//The <div> should have a className="comment" attribute
+//we can unpack variable values directly with JSX by wrapping them in {}, i.e. {this.props.commentText}
+
+
+export default class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
