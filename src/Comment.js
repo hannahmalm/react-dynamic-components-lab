c557ece27abfa0b170bf9b661a39b956c3532612
Hannah Malm <hannahemalm616@gmail.com>
2021-09-11 14:50:27 -0400

    commit tree 4385954d00fde2d09bbafb44afcff174a3e6c3e0

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,17 @@
 //your code here
+import React, { Component } from 'react';
+
+
+//You need to write export default calss or else youll get a warning
+//commentText renders from blog post
+
+
+export default class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
